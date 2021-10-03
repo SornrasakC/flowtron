4717e17ef0e1490ff4e260251fe56d6043f738b1
SornrasakC <43643389+SornrasakC@users.noreply.github.com>
2021-10-03 21:05:24 +0700

    g2p added

--- text/symbols.py
+++ text/symbols.py
@@ -17,9 +17,16 @@ _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
+# 54
+_thai_ipa = ['ô', 'à', 'ŋ', 'h', 'á', 's', '́', '!', '̌', 'w', 'ǒ', 'û', 'ǎ', '̂', 'í', 'ì', 'y', 'ǐ', 'ǔ', 'î', 'c', 'ʉ', 'S', 'l', 'â', 'o', 'é', 't', 'n', 'A', 'ò', 'ɔ', 'a', 'ù', '̀', 'd', 'e', 
+'f', 'I', 'k', 'm', 'i', 'r', 'P', 'è', 'ɛ', 'ě', 'b', 'ê', 'p', 'ə', 'ú', 'ó', 'u']
+
 # Export all symbols:
 # 185 (engs syms) + 88 (thai) = 273
 # symbols = list(_punctuation + _math + _special + _accented + _numbers + _letters) + _arpabet + list(thai_characters)
 
 # (39) + 88 = 127
-symbols = list(_punctuation + _math + _special + _numbers) + list(thai_characters)
+# symbols = list(_punctuation + _math + _special + _numbers) + list(thai_characters)
+
+# 179
+symbols = list(set( list(_punctuation + _math + _special + _numbers) + list(thai_characters) + _thai_ipa ))
